c4548fcbe798eaf869b56063ff634d2d349776f4
ArnaudD - Linux <arnaud.dauguen@orange.fr>
2019-04-16 18:12:54 +0200

    reformat

--- Controllers/add_file_controller.php
+++ Controllers/add_file_controller.php
@@ -8,17 +8,13 @@
         }
 
         $origin_path = $_FILES['file']['tmp_name'];
-
         $hash = hash_file('md5', $origin_path);
-
-
-        $file_already_uploaded = false;
-        if(!empty(AddFile::checkExistingFile($hash))){
-            //file already exist
-            $file_already_uploaded = true;
-        }
         $destination = "./files/" . $hash;
         $_FILES['file']['tmp_name'] = $destination;
+        $potentialFile = AddFile::checkExistingFile($hash);
+
+
+        $file_already_uploaded = !empty($potentialFile);
 
         if(!file_exists("./files")){
             mkdir("./files", 0777, true);
@@ -28,8 +24,9 @@
             AddFile::uploadFile($_FILES['file']['name'], $hash);
             move_uploaded_file($origin_path, $destination);
         }
-        $id_new_file = AddFile::checkExistingFile($hash)['id'];
-        if($id_new_file != null){
+        
+        
+        if($potentialFile['id'] != null){
             AddFile::updateHave(intval($_SESSION['user_id']), $id_new_file);
             Header("Location:" . PATH . "/home");
         }else{
